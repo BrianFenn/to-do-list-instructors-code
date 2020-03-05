c94d0c13db67274135aa6fd87eefd531e5034b60
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-03-04 19:21:11 -0800

    Update database.php

--- model/database.php
+++ model/database.php
@@ -2,10 +2,11 @@
     $dsn = 'erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     $username = 'ps6tp9byxkqga18a';
     $password = 'ikhd47q0kgp7lsz2';
+    $dbname = 'xr7mltv9xu2cv4eo';
 
     try {
         //$db = new PDO($dsn, $username);
-        $db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $dbname, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
