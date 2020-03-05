8e39566ed2d6c875512fce4959b3e324e637a3f3
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-03-04 19:56:08 -0800

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,12 +1,12 @@
 <?php
-    $dsn = 'erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $dsn = 'mysql:host=erxv1bzckceve5lh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com,dbname=xr7mltv9xu2cv4eo';
     $username = 'ps6tp9byxkqga18a';
     $password = 'ikhd47q0kgp7lsz2';
-    $dbname = 'xr7mltv9xu2cv4eo';
+    //$dbname = 'xr7mltv9xu2cv4eo';
 
     try {
         //$db = new PDO($dsn, $username);
-        $db = new PDO($dsn, $dbname, $username, $password);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
